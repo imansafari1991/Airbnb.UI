3c41feae895e93390f95b23df7d3b82259c4c8a2
nastaran <nastaran.reshadati1376@gmail.com>
2024-12-29 09:17:41 -0800

    Add styles for Calendar component

--- app/components/Header/Calendar/CalendarUI/CalendarUi.tsx
+++ app/components/Header/Calendar/CalendarUI/CalendarUi.tsx
@@ -2,29 +2,28 @@
 
 import React, { useState } from 'react';
 import DatePicker from 'react-multi-date-picker';
-import {CalendarFooter} from '../Footer/CalendarFooter'
+import { CalendarFooter } from '../Footer/CalendarFooter';
 
-export const CalendarUi =()=> {
-  const [value, setValue] = useState([]);
-
-  return (
-    <div className="w-full mx-auto bg-slate-500 my-fullwidth-calendar text-center">
-      <DatePicker
-        value={value}
-        onChange={setValue}
-        range
-        rangeHover
-        numberOfMonths={2}
-        monthYearSeparator=" "
-        highlightToday={false}
-        placeholder="click to open"
-        containerStyle={{ width: '30%' }}
-        style={{ width: '100%'}}
-        portal={false}
-        plugins={[
-          <CalendarFooter key="customFooter" position="bottom" />,
-        ]}
-      />
-    </div>
-  );
+export const CalendarUi = () => {
+    const [value, setValue] = useState([]);
+    return (
+        <div className="w-full justify-center bg-slate-500">
+            <DatePicker
+                value={value}
+                onChange={setValue}
+                range
+                rangeHover
+                numberOfMonths={2}
+                monthYearSeparator=" "
+                highlightToday={false}
+                placeholder="click to open"
+                containerStyle={{ width: '100%' }}
+                style={{ width: '100% !important' }}
+                minDate={new Date()}
+                plugins={[
+                    <CalendarFooter key="customFooter" position="bottom" />,
+                ]}
+            />
+        </div>
+    );
 }
