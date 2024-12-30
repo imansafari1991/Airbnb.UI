572c1b5aee64f47d77d7a610f3d87eeed98eda33
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-12-30 22:56:03 +0330

    refactor: add tailwind folder to for tailwind styles of components

--- app/components/list/ListCard.tsx
+++ app/components/list/ListCard.tsx
@@ -7,7 +7,7 @@ import { ROOM_CARD_DATA } from "@/constants";
 const ListCard: React.FC = () => {
   return (
     <div className="w-full h-full flex justify-center items-center">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 px-6 py-6 w-full gap-y-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-4 px-6 py-6 w-full gap-y-10">
         {ROOM_CARD_DATA?.map((roomData) => (
           <RoomCard
             key={roomData?.id}
