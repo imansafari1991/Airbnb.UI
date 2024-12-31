8254a1b2b53d061015d314737def60bf4a74fe3b
hanie-hhh <109070037+hanie-hhh@users.noreply.github.com>
2025-01-01 01:11:22 +0330

    start navigation

--- app/components/navigation/NavigationTag.tsx
+++ app/components/navigation/NavigationTag.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import type React from "react";
 
 const NavigationTag: React.FC = () => {
-    return (
-        <div className="w-full h-20 bg-gray-800 flex items-center justify-center">
-            <span className="text-white">Navigation Tag</span>
-        </div>
-    );
+  return (
+    <nav className="w-screen flex items-center px-0 md:px-10 2xl:px-20 gap-4"></nav>
+  );
 };
 
 export default NavigationTag;
