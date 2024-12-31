b202aa11b97e447cd5166d732893a658f567c19a
hanie-hhh <109070037+hanie-hhh@users.noreply.github.com>
2025-01-01 01:47:35 +0330

    add filter button

--- app/components/navigation/NavigationTag.tsx
+++ app/components/navigation/NavigationTag.tsx
@@ -1,8 +1,15 @@
+import { FilterBtn } from "./filter-btn";
+import { FilterIcon } from "@/app/components/navigation/icons";
 import type React from "react";
 
 const NavigationTag: React.FC = () => {
   return (
-    <nav className="w-screen flex items-center px-0 md:px-10 2xl:px-20 gap-4"></nav>
+    <nav className="w-screen flex items-center px-0 md:px-10 2xl:px-20 gap-4">
+      <FilterBtn>
+        <FilterIcon />
+        <span>Filters</span>
+      </FilterBtn>
+    </nav>
   );
 };
 
