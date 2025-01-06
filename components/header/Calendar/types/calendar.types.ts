e842e66c65671331771adcda0450c9fb4b032527
Negar Babaei <negarbabaei377@gmail.com>
2025-01-06 20:11:27 +0330

    feat: File types were created for the calendar

---
+++ components/header/Calendar/types/calendar.types.ts
@@ -0,0 +1,23 @@
+interface DateValueT {
+    day: number;
+    month: { name: string; shortName: string; number: number };
+    year: number;
+}
+
+export interface CheckInOutValuesT {
+    checkIn: DateValueT | null;
+    checkOut: DateValueT | null;
+}
+
+export type ChecksT ={
+    day: number;
+    month: { name: string; shortName: string; number: number };
+    year: number;
+}
+
+export interface CalendarPropsT {
+    onDateSelect: (dates: {
+        checkIn: { day: number; month: object; year: number };
+        checkOut: { day: number; month: object; year: number };
+    }) => void;
+}
