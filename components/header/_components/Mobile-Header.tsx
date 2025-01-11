221cde1a68801050e8f731436ad565027f57e4f0
lida Gholamian <ta.gholamian@gmail.com>
2025-01-11 17:09:55 +0330

    add style

--- components/header/_components/Mobile-Header.tsx
+++ components/header/_components/Mobile-Header.tsx
@@ -3,7 +3,7 @@ import { CiSearch } from "react-icons/ci";
 export const MobileHeader: React.FC = () => {
   return (
     <div className="flex justify-center items-center">
-      <CiSearch />
+      <CiSearch className="text-neutral-800" />
       <input
         type="text"
         placeholder="Statr your search"
