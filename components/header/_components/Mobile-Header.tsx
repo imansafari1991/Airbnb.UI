8122f3cb30e4c42e4ac34b9dbe1f75a7102c7876
lida Gholamian <ta.gholamian@gmail.com>
2025-01-11 19:13:49 +0330

    add mobile responsive component

--- components/header/_components/Mobile-Header.tsx
+++ components/header/_components/Mobile-Header.tsx
@@ -3,6 +3,7 @@ import { CiSearch } from "react-icons/ci";
 export const MobileHeader: React.FC = () => {
   return (
     <div className="flex justify-center items-center">
+      <h2>responsive header</h2>
       <CiSearch className="text-neutral-800" />
       <input
         type="text"
