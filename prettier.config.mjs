24c82554781343e07faadc15c05dd757a70f578f
mohammad khakshoor <mkhbeats@gmail.com>
2025-01-14 23:44:06 +0330

    feat: add prettier and tailwind class sorting lib

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+
+export default {
+  semi: true,
+  singleQuote: true,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  tabWidth: 2,
+  printWidth: 80,
+  arrowParens: "always",
+  endOfLine: "lf",
+};
